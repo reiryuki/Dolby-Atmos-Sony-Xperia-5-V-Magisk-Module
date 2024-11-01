854e94676011688fb6014a7eea9de1cc18cbde8c
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2024-11-01 13:59:28 +0700

    v0.1

---
+++ uninstall.sh
@@ -0,0 +1,23 @@
+mount -o rw,remount /data
+[ ! "$MODPATH" ] && MODPATH=${0%/*}
+[ ! "$MODID" ] && MODID=`basename "$MODPATH"`
+UID=`id -u`
+
+# log
+exec 2>/data/media/"$UID"/$MODID\_uninstall.log
+set -x
+
+# run
+. $MODPATH/function.sh
+
+# cleaning
+remove_cache
+PKGS=`cat $MODPATH/package.txt`
+for PKG in $PKGS; do
+  rm -rf /data/user*/"$UID"/$PKG
+done
+remove_sepolicy_rule
+rm -f /data/vendor/dolby/dax_sqlite3.db
+resetprop -p --delete persist.vendor.dolby.loglevel
+
+
